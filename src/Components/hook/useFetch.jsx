93d8e37c08195e1954eaa7b08cdd691ad75f4da4
Ghanu2204 <ghanshyamsojitra173@gmail.com>
2024-07-18 11:08:16 +0530

    changes in fetch

--- src/Components/hook/useFetch.jsx
+++ src/Components/hook/useFetch.jsx
@@ -1,107 +1,71 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-const useFetch = (url) => {
-  const [data, setData] = useState([]);
-  const [filterData, setFilterData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [activeData, setActiveData] = useState(false);
-  const [page, setPage] = useState(1);
-  const [search, setSearch] = useState("");
-  const [location, setLocation] = useState("");
-  const itemsperPage = 10;
+const useFetch = (filters, disableAutoFetch = false) => {
+  const [tableData, setTableData] = useState({
+    isLoading: false,
+    data: [],
+    isError: false,
+    errorMessage: "",
+  });
 
-  const fetchData = async () => {
-    setIsLoading(true);
-    setIsError(false);
+  const { data, errorMessage, isError, isLoading } = tableData;
+
+  const getData = useCallback(async () => {
+    setTableData((prev) => ({
+      ...prev,
+      isLoading: true,
+      isError: false,
+      errorMessage: "",
+      data: [],
+    }));
     try {
-      const response = await fetch(url);
-      const result = await response.json();
-      setData(result.data);
-      setFilterData(result.data);
+      const { search, location, isActive } = filters;
+      let url = "https://dev.carzup.in/api/pricelist/test-mock";
+      if (isActive) {
+        url += `?active=${isActive}`;
+      }
+      if (search) {
+        {
+          isActive ? (url += `&search=${search}`) : (url += `?search=${search}`);
+        }
+      }
+      if (location) {
+        {
+          isActive ? (url += `&location=${location}`) : (url += `?location=${location}`);
+        }
+      }
+      const dt = await fetch(url, {
+        method: "GET",
+      });
+      const jsonDt = await dt.json();
+      setTableData((prev) => ({
+        ...prev,
+        isLoading: false,
+        data: Array.from(jsonDt.data),
+      }));
     } catch (error) {
-      setIsError(true);
+      setTableData((prev) => ({
+        ...prev,
+        isLoading: false,
+        isError: false,
+        errorMessage: "",
+      }));
+      console.log(error);
     }
-    setIsLoading(false);
-  };
-
-  const handleactiveData = () => {
-    setActiveData((preActive) => !preActive);
-  };
-
-  const handleSwitchChange = (index) => {
-    const updatedData = data.filter((item) => {
-      if (item === index) {
-        item.active = !item.active;
-        return true;
-      }
-      return true;
-    });
-    setFilterData(updatedData);
-  };
-
-  const startIndex = (page - 1) * itemsperPage;
-  const endIndex = startIndex + itemsperPage;
-  const totalPage = Math.ceil(filterData.length / itemsperPage);
-  const currentPage = filterData.slice(startIndex, endIndex);
-
-  const handlePageChange = (selectedPage) => {
-    setPage(selectedPage);
-  };
+  }, [filters]);
 
   useEffect(() => {
-    setPage(1);
-    fetchData();
-  }, [search, location, activeData]);
-
-  useEffect(() => {
-    const filterdata = () => {
-      let newData = data;
-      if (search.trim()) {
-        newData = newData.filter((item) =>
-          item.name.toLowerCase().includes(search.toLowerCase().trim())
-        );
-      }
-
-      if (location.trim()) {
-        newData = newData.filter((item) => item.location === location);
-      }
-
-      if (activeData) {
-        newData = newData.filter((item) => {
-          if (activeData) {
-            if (item.active === true) {
-              return true;
-            } else {
-              return false;
-            }
-          }
-          return true;
-        });
-      }
-
-      setFilterData(newData);
-    };
-    filterdata();
-  }, [search, location, activeData, data]);
+    if (!disableAutoFetch) {
+      getData();
+    }
+  }, [disableAutoFetch, getData]);
 
   return {
-    data,
-    isLoading,
     isError,
-    handleSwitchChange,
-    handleactiveData,
-    activeData,
-    currentPage,
-    setPage,
-    page,
-    totalPage,
-    itemsperPage,
-    handlePageChange,
-    search,
-    location,
-    setSearch,
-    setLocation,
+    isLoading,
+    data,
+    errorMessage,
+    getData,
   };
 };
 
